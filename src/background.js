42ac1022ef5cf4c81958c2efca479ccb3f4711a2
NameCrisis <ryanhdz@proton.me>
2024-02-17 19:50:08 -0800

    Messing around with background and popup js

--- src/background.js
+++ src/background.js
@@ -1,7 +1,27 @@
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      files: ['content.js']
-    });
-  });
+chrome.runtime.onMessage.addListener(
+    function(request,sender,sendResponse)
+    {
+        if (message.request == "GO"){
+            saveHtml();
+        }
+    }
+)
+
+function saveHtml() {
+    const html = document.documentElement.outerHTML;
+    //  the logic to save HTML 
+    function saveHtmlToStorage() {
+        const html = document.documentElement.outerHTML;
+        chrome.storage.local.set({key: html}, function() {
+          console.log('HTML is saved in Chrome storage');
+        });
+      }
+  }
+
+// chrome.action.onClicked.addListener((tab) => {
+//     chrome.scripting.executeScript({
+//       target: {tabId: tab.id},
+//       files: ['content.js']
+//     });
+//   });
   
