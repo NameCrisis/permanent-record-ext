46131fa2db682fe06d9221d8bcd1557a8a45e959
Joseph Nguyen <62458797+jspnguyen@users.noreply.github.com>
2024-02-18 07:59:34 -0800

    Add files via upload

--- src/content.js
+++ src/content.js
@@ -1,32 +1,9 @@
-/* File: content.js
- * ---------------
- * Hello! You'll be making most of your changes
- * in this file. At a high level, this code replaces
- * the substring "cal" with the string "butt" on web pages.
- *
- * This file contains javascript code that is executed
- * everytime a webpage loads over HTTP or HTTPS.
- */
+function checkForWords(wordsArray) {
+    const bodyText = document.body.innerText || "";
+    return wordsArray.some(word => bodyText.includes(word));
+}
 
-// const archivedPage = document.documentElement.outerHTML;
+const specificWords = ['searching', 'something', 'else', 'exist'];
+const found = checkForWords(specificWords);
 
-// // document.body.addEventListener("DOMContentLoaded", savePage())
-
-// function savePage(){
-//     print(archivedPage)
-// }
-
-(function() {
-    const html = document.documentElement.outerHTML;
-    // Here, you can either display the HTML directly,
-    // send it to a server, or use the Chrome Storage API to save it.
-    console.log(html);
-    function saveHtmlToStorage() {
-        const html = document.documentElement.outerHTML;
-        chrome.storage.local.set({key: html}, function() {
-          console.log('HTML is saved in Chrome storage');
-        });
-      }
-        })();
-
-        
+chrome.runtime.sendMessage({found: found});
